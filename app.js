51c41d239441efe62aea1d2be2c926aa70c6b462
primehalit <118754220+primehalit@users.noreply.github.com>
2023-10-26 12:49:55 +0300

    Prime

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('MERHABA BEN ÜBEYDULLAH CEKLİ!');
+  res.send('MERHABA BEN HALİT MIZRAK SANAL EVRENDEKİ DOSTLARIMA  SESLENİYORUM BİZ BURDAYIZ SİZİ BEKLİYORUZ!');
 });
 
 const port = process.env.PORT || 3000;
